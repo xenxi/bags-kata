841afc4c2e7ccfe2aaebc7439071378642f135ae
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-20 21:07:49 +0100

    :white_check_mark: green test: print_one_item_in_the_backpack

--- BagsKataDotNet/BagKata.Test/CharacterShould.cs
+++ BagsKataDotNet/BagKata.Test/CharacterShould.cs
@@ -52,6 +52,18 @@ namespace BagKata.Test
             printer.Received(1).Print("backpack = ['Space Tomato']");
         }
 
+        [TestCase("Space Hamster", "backpack = ['Space Hampster']")]
+        [TestCase("Space Tomato", "backpack = ['Space Tomato']")]
+        public void print_one_item_in_the_backpack(string item, string printedBackpack)
+        {
+            var printer = Substitute.For<IPrinter>();
+            var durance = new Character(printer);
+            durance.Add(item);
+
+            durance.PrintInventory();
+
+            printer.Received(1).Print(printedBackpack);
+        }
 
     }
 }
