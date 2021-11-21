e6da446d93879a51b7edb4e9fcdd7790ff31c55b
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-21 12:18:50 +0100

    :recycle: refact introduce fields

--- BagsKataDotNet/BagKata.Test/CharacterShould.cs
+++ BagsKataDotNet/BagKata.Test/CharacterShould.cs
@@ -6,65 +6,62 @@ namespace BagKata.Test
     [TestFixture]
     public class CharacterShould
     {
+        private IBackpack _backpack;
+        private Character _durance;
+        private IPrinter _printer;
         [Test]
         public void add_item_to_the_backpack()
         {
-            var printer = Substitute.For<IPrinter>();
-            var backpack = Substitute.For<IBackpack>();
-            var durance = new Character(printer, backpack);
             var aGivenAnyItem = "anyItem";
 
-            durance.Add(aGivenAnyItem);
+            _durance.Add(aGivenAnyItem);
 
-            backpack.Received(1).Add(aGivenAnyItem);
+            _backpack.Received(1).Add(aGivenAnyItem);
         }
 
         [Test]
         public void print_empty_inventory()
         {
-            var printer = Substitute.For<IPrinter>();
-            var backpack = Substitute.For<IBackpack>();
-            var durance = new Character(printer, backpack);
-
-            durance.PrintInventory();
+            _durance.PrintInventory();
 
             Received.InOrder(() =>
             {
-                printer.Print("backpack = []");
-                printer.Print("bag_with_metals_category = []");
-                printer.Print("bag_with_no_category = []");
-                printer.Print("bag_with_weapons_category = []");
-                printer.Print("bag_with_no_category = []");
+                _printer.Print("backpack = []");
+                _printer.Print("bag_with_metals_category = []");
+                _printer.Print("bag_with_no_category = []");
+                _printer.Print("bag_with_weapons_category = []");
+                _printer.Print("bag_with_no_category = []");
             });
-
         }
-
 
         [TestCase("Space Hampster", "backpack = ['Space Hampster']")]
         [TestCase("Space Tomato", "backpack = ['Space Tomato']")]
         public void print_one_item_in_the_backpack(string item, string printedBackpack)
         {
-            var printer = Substitute.For<IPrinter>();
-            var backpack = Substitute.For<IBackpack>();
-            var durance = new Character(printer, backpack);
-            durance.Add(item);
+            _durance.Add(item);
 
-            durance.PrintInventory();
+            _durance.PrintInventory();
 
-            printer.Received(1).Print(printedBackpack);
+            _printer.Received(1).Print(printedBackpack);
         }
+
         [Test]
         public void print_two_items_in_the_backpack()
         {
-            var printer = Substitute.For<IPrinter>();
-            var backpack = Substitute.For<IBackpack>();
-            var durance = new Character(printer, backpack);
-            durance.Add("anyItem");
-            durance.Add("otherItem");
+            _durance.Add("anyItem");
+            _durance.Add("otherItem");
 
-            durance.PrintInventory();
+            _durance.PrintInventory();
 
-            printer.Received(1).Print("backpack = ['anyItem', 'otherItem']");
+            _printer.Received(1).Print("backpack = ['anyItem', 'otherItem']");
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _printer = Substitute.For<IPrinter>();
+            _backpack = Substitute.For<IBackpack>();
+            _durance = new Character(_printer, _backpack);
         }
     }
 }
