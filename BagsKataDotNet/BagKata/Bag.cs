24eb46c0c5138bcddc47ec8fb422345d42d1148f
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-21 20:47:40 +0100

    :recycle: refact extract constant

--- BagsKataDotNet/BagKata/Bag.cs
+++ BagsKataDotNet/BagKata/Bag.cs
@@ -5,6 +5,7 @@ namespace BagKata
 {
     public class Bag : IBag
     {
+        private const int Capacity = 4;
         private readonly List<string> _items = new List<string>();
 
         public void Add(string item)
@@ -20,7 +21,7 @@ namespace BagKata
                 throw new InvalidOperationException("the bag is complete");
         }
 
-        public int FreeSlots() => 4 - _items.Count;
+        public int FreeSlots() => Capacity - _items.Count;
         public IEnumerable<string> GetItems() => _items.AsReadOnly();
 
         public bool IsFull() => FreeSlots() < 1;
