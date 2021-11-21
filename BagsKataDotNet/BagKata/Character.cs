e1ececea46640dcc9475dfdb5f07bfdbc58bb099
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-21 14:56:20 +0100

    :recycle: refact inline field

--- BagsKataDotNet/BagKata/Character.cs
+++ BagsKataDotNet/BagKata/Character.cs
@@ -4,19 +4,15 @@
     {
         private readonly IInventory _inventory;
         private readonly InventoryPrinter _inventoryPrinter;
-        private readonly IPrinter _printer;
+
         public Character(IPrinter printer, IInventory inventory)
         {
-            _printer = printer;
             _inventory = inventory;
-            _inventoryPrinter = new InventoryPrinter(_printer);
+            _inventoryPrinter = new InventoryPrinter(printer);
         }
 
         public void Add(string leather) => _inventory.Add(leather);
 
-        public void PrintInventory()
-        {
-            _inventoryPrinter.printInventory(_inventory);
-        }
+        public void PrintInventory() => _inventoryPrinter.printInventory(_inventory);
     }
 }
