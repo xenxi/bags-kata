e6781bb3a779be764e2f618d2c3adedc7af8d90b
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-21 15:09:13 +0100

    :fire: remove duplicate tests

--- BagsKataDotNet/BagKata.Test/CharacterShould.cs
+++ BagsKataDotNet/BagKata.Test/CharacterShould.cs
@@ -13,35 +13,6 @@ namespace BagKata.Test
         private IPrinter _printer;
 
         [Test]
-        public void add_item_to_the_backpack()
-        {
-            var aGivenAnyItem = "anyItem";
-
-            _durance.Add(aGivenAnyItem);
-
-            _backpack.Received(1).Add(aGivenAnyItem);
-        }
-
-        [Test]
-        public void add_item_to_the_next_bag_when_previous_ones_are_full()
-        {
-            var aGivenAnyItem = "anyItem";
-            _backpack.IsFull().Returns(true);
-            _bag.IsFull().Returns(true);
-            _durance.Add(aGivenAnyItem);
-
-            _secondBag.Received(1).Add(aGivenAnyItem);
-        }
-        [Test]
-        public void add_item_to_the_bag_when_backpack_is_full()
-        {
-            var aGivenAnyItem = "anyItem";
-            _backpack.IsFull().Returns(true);
-            _durance.Add(aGivenAnyItem);
-
-            _bag.Received(1).Add(aGivenAnyItem);
-        }
-        [Test]
         public void print_empty_inventory()
         {
             _durance.PrintInventory();
