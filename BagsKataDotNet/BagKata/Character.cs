38f745197ee197bea0268a01c9ae1abb2781423f
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-21 14:55:11 +0100

    :recycle: refact extract  class

--- BagsKataDotNet/BagKata/Character.cs
+++ BagsKataDotNet/BagKata/Character.cs
@@ -1,39 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace BagKata
+﻿namespace BagKata
 {
     public class Character
     {
         private readonly IInventory _inventory;
+        private readonly InventoryPrinter _inventoryPrinter;
         private readonly IPrinter _printer;
-
         public Character(IPrinter printer, IInventory inventory)
         {
             _printer = printer;
             _inventory = inventory;
+            _inventoryPrinter = new InventoryPrinter(_printer);
         }
 
         public void Add(string leather) => _inventory.Add(leather);
 
         public void PrintInventory()
         {
-            printInventory(_printer, _inventory, new InventoryPrinter());
+            _inventoryPrinter.printInventory(_inventory);
         }
-
-        private static void printInventory(IPrinter printer, IInventory inventory, InventoryPrinter inventoryPrinter)
-        {
-            IEnumerable<string> items = inventory.GetItems();
-            printer.Print($"backpack = [{string.Join(", ", items.Select(x => $"'{x}'"))}]");
-
-            printer.Print("bag_with_metals_category = []");
-            printer.Print("bag_with_no_category = []");
-            printer.Print("bag_with_weapons_category = []");
-            printer.Print("bag_with_no_category = []");
-        }
-    }
-
-    public class InventoryPrinter
-    {
     }
 }
---
+++ BagsKataDotNet/BagKata/InventoryPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BagKata
+{
+    public class InventoryPrinter
+    {
+        private readonly IPrinter _printer;
+
+        public InventoryPrinter(IPrinter printer)
+        {
+            _printer = printer;
+        }
+
+        public void printInventory(IInventory inventory)
+        {
+            IEnumerable<string> items = inventory.GetItems();
+
+            _printer.Print($"backpack = [{string.Join(", ", items.Select(x => $"'{x}'"))}]");
+
+            _printer.Print("bag_with_metals_category = []");
+            _printer.Print("bag_with_no_category = []");
+            _printer.Print("bag_with_weapons_category = []");
+            _printer.Print("bag_with_no_category = []");
+        }
+    }
+}
