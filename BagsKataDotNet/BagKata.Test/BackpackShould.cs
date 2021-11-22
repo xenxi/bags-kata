446e81fefa617e51a321046f22eef86dc451f2bd
xenxi <antoniom.diaz.moreno@gmail.com>
2021-11-22 06:56:41 +0100

    :white_check_mark: add test for backpack

---
+++ BagsKataDotNet/BagKata.Test/BackpackShould.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BagKata.Test
+{
+    [TestFixture]
+    public class BackpackShould
+    {
+        [Test]
+        public void store_a_item()
+        {
+            var aGivenItem = "anyItem";
+            var bag = new Backpack();
+
+            bag.Add(aGivenItem);
+
+            bag.GetItems().Should().ContainSingle(aGivenItem);
+        }
+        [Test]
+        public void has_4_free_slots_when_its_empty()
+        {
+            var bag = new Backpack();
+            
+            bag.FreeSlots().Should().Be(8);
+        }
+
+        [Test]
+        public void be_not_full_when_its_empty()
+        {
+            var bag = new Backpack();
+
+            bag.IsFull().Should().BeFalse();
+        }
+        [Test]
+        public void has_7_free_slots_when_add_1_item()
+        {
+            var bag = new Backpack();
+
+            bag.Add("anyItem");
+
+            bag.FreeSlots().Should().Be(7);
+        }
+
+        [Test]
+        public void has_0_free_slots_when_add_8_items()
+        {
+            var bag = new Backpack();
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+
+            bag.Add("anyItem");
+
+            bag.FreeSlots().Should().Be(0);
+        }
+
+        [Test]
+        public void be_full_when_not_have_free_slots()
+        {
+            var bag = new Backpack();
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+
+            bag.Add("anyItem");
+
+            bag.IsFull().Should().BeTrue();
+        }
+
+        [Test]
+        public void no_allow_add_new_item_when_its_full()
+        {
+            var bag = new Backpack();
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+            bag.Add("anyItem");
+
+            Action action = () => bag.Add("anyOtherItem");
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
---
+++ BagsKataDotNet/BagKata/Backpack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BagKata
+{
+    public class Backpack : IBag
+    {
+        private const int Capacity = 8;
+        private readonly List<string> _items = new List<string>();
+
+        public void Add(string item)
+        {
+            EnsureHasFreeSlots();
+
+            _items.Add(item);
+        }
+
+        private void EnsureHasFreeSlots()
+        {
+            if (IsFull())
+                throw new InvalidOperationException("the backpack is complete");
+        }
+
+        public int FreeSlots() => Capacity - _items.Count;
+        public IEnumerable<string> GetItems() => _items.AsReadOnly();
+
+        public bool IsFull() => FreeSlots() < 1;
+    }
+}
